e2399fe92e8b5a864d942e09582392b8c8e1e148
Andy Piper <github@andypiper.com>
2023-10-01 18:21:01 +0100

    Blimp: update for new AC_PID settings
    DFF logging docs

--- Blimp/Parameters.cpp
+++ Blimp/Parameters.cpp
@@ -730,12 +730,6 @@ const AP_Param::Info Blimp::var_info[] = {
     // @Units: d%
     // @User: Advanced
 
-    // @Param: POSYAW_ADV
-    // @DisplayName: Advanced Yaw parameters enable
-    // @Description: Advanced Yaw parameters enable
-    // @Values: 0:Disabled,1:Enabled
-    // @User: Advanced
-
     // @Param: POSYAW_D_FF
     // @DisplayName: Position (yaw) Derivative FeedForward Gain
     // @Description: FF D Gain which produces an output that is proportional to the rate of change of the target
@@ -744,31 +738,15 @@ const AP_Param::Info Blimp::var_info[] = {
     // @User: Advanced
 
     // @Param: POSYAW_NTF
-    // @DisplayName: Position (yaw) Target notch Filter center frequency
-    // @Description: Position (yaw) Target notch Filter center frequency in Hz.
-    // @Range: 10 495
-    // @Units: Hz
+    // @DisplayName: Position (yaw) Target notch filter index
+    // @Description: Position (yaw) Target notch filter index
+    // @Range: 1 8
     // @User: Advanced
 
     // @Param: POSYAW_NEF
-    // @DisplayName: Position (yaw) Error notch Filter center frequency
-    // @Description: Position (yaw) Error notch Filter center frequency in Hz.
-    // @Range: 10 495
-    // @Units: Hz
-    // @User: Advanced
-
-    // @Param: POSYAW_NBW
-    // @DisplayName: Position (yaw) notch Filter bandwidth
-    // @Description: Position (yaw) notch Filter bandwidth in Hz.
-    // @Range: 5 250
-    // @Units: Hz
-    // @User: Advanced
-
-    // @Param: POSYAW_NATT
-    // @DisplayName: Position (yaw) notch Filter attenuation
-    // @Description: Position (yaw) notch Filter attenuation in dB.
-    // @Range: 5 50
-    // @Units: dB
+    // @DisplayName: Position (yaw) Error notch filter index
+    // @Description: Position (yaw) Error notch filter index
+    // @Range: 1 8
     // @User: Advanced
 
     GOBJECT(pid_pos_yaw, "POSYAW_", AC_PID),
