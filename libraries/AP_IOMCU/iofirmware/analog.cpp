4321c17f0f1a36a348e78f04e33ce4e165ebb4dc
Andy Piper <github@andypiper.com>
2023-11-13 19:58:36 +0000

    AP_IOMCU: constrain PWM channels to 8, telem channels to 4 and RC channels to 16
    make ADC readings interrupt driven
    turn off iomcu updates when debugging
    allow for correct number of telemetry channels

--- libraries/AP_IOMCU/iofirmware/analog.cpp
+++ libraries/AP_IOMCU/iofirmware/analog.cpp
@@ -13,9 +13,9 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 /*
-  analog capture for IOMCU. This uses direct register access to avoid
-  using up a DMA channel and to minimise latency. We capture a single
-  sample at a time
+   analog capture for IOMCU. This uses direct register access to avoid
+   using up a DMA channel and to minimise latency. We capture a single
+   sample at a time
  */
 
 #include "ch.h"
@@ -26,13 +26,37 @@
 #error "HAL_USE_ADC must be set"
 #endif
 
+extern "C" {
+    extern void Vector88();
+}
+
+#define STM32_ADC1_NUMBER 18
+#define STM32_ADC1_HANDLER Vector88
+
+const uint32_t VSERVO_CHANNEL = ADC_SQR3_SQ1_N(ADC_CHANNEL_IN4);
+const uint32_t VRSSI_CHANNEL = ADC_SQR3_SQ1_N(ADC_CHANNEL_IN5);
+
+static uint16_t vrssi_val = 0xFFFF;
+static uint16_t vservo_val = 0xFFFF;
+
 /*
   initialise ADC capture
  */
 void adc_init(void)
 {
-    adc_lld_init();
     rccEnableADC1(true);
+    ADC1->CR1 = 0;
+    ADC1->CR2 = ADC_CR2_ADON;
+
+    /* Reset calibration just to be safe.*/
+    ADC1->CR2 = ADC_CR2_ADON | ADC_CR2_RSTCAL;
+    while ((ADC1->CR2 & ADC_CR2_RSTCAL) != 0)
+      ;
+
+    /* Calibration.*/
+    ADC1->CR2 = ADC_CR2_ADON | ADC_CR2_CAL;
+    while ((ADC1->CR2 & ADC_CR2_CAL) != 0)
+      ;
 
     /* set channels 4 and 5 for 28.5us sample time */
     ADC1->SMPR2 = ADC_SMPR2_SMP_AN4(ADC_SAMPLE_28P5) | ADC_SMPR2_SMP_AN5(ADC_SAMPLE_28P5);
@@ -40,33 +64,27 @@ void adc_init(void)
     /* capture a single sample at a time */
     ADC1->SQR1 = 0;
     ADC1->SQR2 = 0;
+
+    ADC1->CR1 |= ADC_CR1_EOCIE;
+
+    nvicEnableVector(STM32_ADC1_NUMBER, STM32_ADC_ADC1_IRQ_PRIORITY);
 }
 
 /*
   capture one sample on a channel
  */
-static uint16_t adc_sample_channel(uint32_t channel)
+static void adc_sample_channel(uint32_t channel)
 {
-    // clear EOC
-    ADC1->SR = 0;
+    chSysLock();
 
-    /* capture one sample */
-    ADC1->SQR3 = channel;
-    ADC1->CR2 |= ADC_CR2_ADON;
-
-    /* wait for the conversion to complete */
-    uint32_t counter = 16;
-
-    while (!(ADC1->SR & ADC_SR_EOC)) {
-        if (--counter == 0) {
-            // ensure EOC is clear
-            ADC1->SR = 0;
-            return 0xffff;
-        }
+    if (ADC1->SR & ADC_SR_STRT) {
+        return; // still waiting for sample
     }
 
-    // return sample (this also clears EOC flag)
-    return ADC1->DR;
+    /* capture another sample */
+    ADC1->SQR3 = channel;
+    ADC1->CR2 |= ADC_CR2_ADON;
+    chSysUnlock();
 }
 
 /*
@@ -74,8 +92,8 @@ static uint16_t adc_sample_channel(uint32_t channel)
  */
 uint16_t adc_sample_vservo(void)
 {
-    const uint32_t channel = ADC_SQR3_SQ1_N(ADC_CHANNEL_IN4);
-    return adc_sample_channel(channel);
+    adc_sample_channel(VSERVO_CHANNEL);
+    return vservo_val;
 }
 
 /*
@@ -83,6 +101,30 @@ uint16_t adc_sample_vservo(void)
  */
 uint16_t adc_sample_vrssi(void)
 {
-    const uint32_t channel = ADC_SQR3_SQ1_N(ADC_CHANNEL_IN5);
-    return adc_sample_channel(channel);
+    adc_sample_channel(VRSSI_CHANNEL);
+    return vrssi_val;
+}
+
+static void adc_read_sample()
+{
+    if (ADC1->SR & ADC_SR_EOC) {
+        if (ADC1->SQR3 == VSERVO_CHANNEL) {
+            vservo_val = ADC1->DR;
+        } else if (ADC1->SQR3 == VRSSI_CHANNEL) {
+            vrssi_val = ADC1->DR;
+        }
+        ADC1->SR &= ~(ADC_SR_EOC | ADC_SR_STRT);
+    }
+}
+
+OSAL_IRQ_HANDLER(STM32_ADC1_HANDLER) {
+    OSAL_IRQ_PROLOGUE();
+
+    chSysLockFromISR();
+
+    adc_read_sample();
+
+    chSysUnlockFromISR();
+
+    OSAL_IRQ_EPILOGUE();
 }
