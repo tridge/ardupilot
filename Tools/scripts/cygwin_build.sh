7d74b2837c3cd1902a60ee18282c7a4721b00494
Lokesh-Ramina <loki077@gmail.com>
2023-11-20 20:45:13 -0800

    CI: Carbonix disable features not required
    This test would be difficult to port to a private repo or are not needed and making our CI take more longer time.
    - macos not required
    - test_environment not required
    - test_size not required
    - Cygwin not build except for plane

--- Tools/scripts/cygwin_build.sh
+++ Tools/scripts/cygwin_build.sh
@@ -25,25 +25,25 @@ mkdir artifacts
 (
     python ./waf --color yes --toolchain $TOOLCHAIN --board sitl configure 2>&1
     python ./waf plane 2>&1
-    python ./waf copter 2>&1
-    python ./waf heli 2>&1
-    python ./waf rover 2>&1
-    python ./waf sub 2>&1
 ) | tee artifacts/build.txt
+    # python ./waf copter 2>&1
+    # python ./waf heli 2>&1
+    # python ./waf rover 2>&1
+    # python ./waf sub 2>&1
 
 # copy both with exe and without to cope with differences
 # between windows versions in CI
 cp -v build/sitl/bin/arduplane artifacts/ArduPlane.elf.exe
-cp -v build/sitl/bin/arducopter artifacts/ArduCopter.elf.exe
-cp -v build/sitl/bin/arducopter-heli artifacts/ArduHeli.elf.exe
-cp -v build/sitl/bin/ardurover artifacts/ArduRover.elf.exe
-cp -v build/sitl/bin/ardusub artifacts/ArduSub.elf.exe
+# cp -v build/sitl/bin/arducopter artifacts/ArduCopter.elf.exe
+# cp -v build/sitl/bin/arducopter-heli artifacts/ArduHeli.elf.exe
+# cp -v build/sitl/bin/ardurover artifacts/ArduRover.elf.exe
+# cp -v build/sitl/bin/ardusub artifacts/ArduSub.elf.exe
 
 cp -v build/sitl/bin/arduplane artifacts/ArduPlane.elf
-cp -v build/sitl/bin/arducopter artifacts/ArduCopter.elf
-cp -v build/sitl/bin/arducopter-heli artifacts/ArduHeli.elf
-cp -v build/sitl/bin/ardurover artifacts/ArduRover.elf
-cp -v build/sitl/bin/ardusub artifacts/ArduSub.elf
+# cp -v build/sitl/bin/arducopter artifacts/ArduCopter.elf
+# cp -v build/sitl/bin/arducopter-heli artifacts/ArduHeli.elf
+# cp -v build/sitl/bin/ardurover artifacts/ArduRover.elf
+# cp -v build/sitl/bin/ardusub artifacts/ArduSub.elf
 
 # Find all cyg*.dll files returned by cygcheck for each exe in artifacts
 # and copy them over
