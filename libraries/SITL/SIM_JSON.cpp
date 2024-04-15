5886e96e8a663225adb67e2464521e8f03edb1bf
AJ <ajcruz@comquestventures.com>
2024-04-15 09:10:19 +0900

    SITL: support SimNet simulator

--- libraries/SITL/SIM_JSON.cpp
+++ libraries/SITL/SIM_JSON.cpp
@@ -12,7 +12,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-/* 
+/*
     Simulator Connector for JSON based interfaces
 */
 
@@ -29,7 +29,6 @@
 #include <AP_HAL/AP_HAL.h>
 #include <AP_Logger/AP_Logger.h>
 #include <AP_HAL/utility/replace.h>
-#include <SRV_Channel/SRV_Channel.h>
 
 #define UDP_TIMEOUT_MS 100
 
@@ -103,34 +102,20 @@ void JSON::set_interface_ports(const char* address, const int port_in, const int
 */
 void JSON::output_servos(const struct sitl_input &input)
 {
-    size_t pkt_size = 0;
-    ssize_t send_ret = -1;
-    if (SRV_Channels::have_32_channels()) {
-      servo_packet_32 pkt;
-      pkt.frame_rate = rate_hz;
-      pkt.frame_count = frame_counter;
-      for (uint8_t i=0; i<32; i++) {
-          pkt.pwm[i] = input.servos[i];
-      }
-      pkt_size = sizeof(pkt);
-      send_ret = sock.sendto(&pkt, pkt_size, target_ip, control_port);
-    } else {
-      servo_packet_16 pkt;
-      pkt.frame_rate = rate_hz;
-      pkt.frame_count = frame_counter;
-      for (uint8_t i=0; i<16; i++) {
-          pkt.pwm[i] = input.servos[i];
-      }
-      pkt_size = sizeof(pkt);
-      send_ret = sock.sendto(&pkt, pkt_size, target_ip, control_port);
+    servo_packet pkt;
+    pkt.frame_rate = rate_hz;
+    pkt.frame_count = frame_counter;
+    for (uint8_t i=0; i<16; i++) {
+        pkt.pwm[i] = input.servos[i];
     }
 
-    if ((size_t)send_ret != pkt_size) {
+    size_t send_ret = sock.sendto(&pkt, sizeof(pkt), target_ip, control_port);
+    if (send_ret != sizeof(pkt)) {
         if (send_ret <= 0) {
             printf("Unable to send servo output to %s:%u - Error: %s, Return value: %ld\n",
                    target_ip, control_port, strerror(errno), (long)send_ret);
         } else {
-            printf("Sent %ld bytes instead of %lu bytes\n", (long)send_ret, (unsigned long)pkt_size);
+            printf("Sent %ld bytes instead of %lu bytes\n", (long)send_ret, (unsigned long)sizeof(pkt));
         }
     }
 }
@@ -339,6 +324,10 @@ void JSON::recv_fdm(const struct sitl_input &input)
         update_eas_airspeed();
     }
 
+    if ((received_bitmask & WIND_VEL) != 0) {
+        wind_ef = state.velocity_wind;
+    }
+
     // Convert from a meters from origin physics to a lat long alt
     update_position();
 
@@ -358,6 +347,22 @@ void JSON::recv_fdm(const struct sitl_input &input)
         wind_vane_apparent.speed = state.wind_vane_apparent.speed;
     }
 
+    // update RC input
+    rcin_chan_count = 10;
+    for (uint8_t i=0; i<rcin_chan_count; i++) {
+        if ((received_bitmask &  1U << (i+18)) != 0) {
+            rcin[i] = (state.rc[i] - 1000.0f) / 1000.0f;
+        }
+    }
+
+    // update battery state
+    if ((received_bitmask & BAT_VOLT) != 0) {
+        battery_voltage = state.bat_volt; 
+    }
+    if ((received_bitmask & BAT_AMP) != 0) {
+        battery_current = state.bat_amp; 
+    }
+
     double deltat;
     if (state.timestamp_s < last_timestamp_s) {
         // Physics time has gone backwards, don't reset AP
@@ -371,9 +376,9 @@ void JSON::recv_fdm(const struct sitl_input &input)
 
     if (is_positive(deltat) && deltat < 0.1) {
         // time in us to hz
-        if (use_time_sync) {
-            adjust_frame_time(1.0 / deltat);
-        }
+        //if (use_time_sync) { // SIMNET: Disable
+        //    adjust_frame_time(1.0 / deltat);
+        //}
         // match actual frame rate with desired speedup
         time_advance();
     }
@@ -462,7 +467,7 @@ void JSON::update(const struct sitl_input &input)
     update_mag_field_bf();
 
     // allow for changes in physics step
-    adjust_frame_time(constrain_float(sitl->loop_rate_hz, rate_hz-1, rate_hz+1));
+    //adjust_frame_time(constrain_float(sitl->loop_rate_hz, rate_hz-1, rate_hz+1)); // SIMNET: Disable
 
 #if 0
     // report frame rate
--- libraries/SITL/SIM_JSON.h
+++ libraries/SITL/SIM_JSON.h
@@ -18,7 +18,7 @@
 
 #if AP_SIM_JSON_ENABLED
 
-#include <AP_HAL/utility/Socket_native.h>
+#include <AP_HAL/utility/Socket.h>
 #include "SIM_Aircraft.h"
 
 namespace SITL {
@@ -40,18 +40,11 @@ public:
 
 private:
 
-    struct servo_packet_16 {
+    struct servo_packet {
         uint16_t magic = 18458; // constant magic value
         uint16_t frame_rate;
         uint32_t frame_count;
         uint16_t pwm[16];
-    };
-
-    struct servo_packet_32 {
-        uint16_t magic = 29569; // constant magic value
-        uint16_t frame_rate;
-        uint32_t frame_count;
-        uint16_t pwm[32];
     };
 
     // default connection_info_.ip_address
@@ -60,7 +53,7 @@ private:
     // default connection_info_.sitl_ip_port
     uint16_t control_port = 9002;
 
-    SocketAPM_native sock;
+    SocketAPM sock;
 
     uint32_t frame_counter;
     double last_timestamp_s;
@@ -94,7 +87,11 @@ private:
         Vector3f attitude;
         Quaternion quaternion;
         Vector3f velocity;
+        Vector3f velocity_wind;
         float rng[6];
+        float rc[12];
+        float bat_volt;
+        float bat_amp;
         struct {
             float direction;
             float speed;
@@ -110,7 +107,7 @@ private:
         void *ptr;
         enum data_type type;
         bool required;
-    } keytable[17] = {
+    } keytable[30] = {
         { "", "timestamp", &state.timestamp_s, DATA_DOUBLE, true },
         { "imu", "gyro",    &state.imu.gyro, DATA_VECTOR3F, true },
         { "imu", "accel_body", &state.imu.accel_body, DATA_VECTOR3F, true },
@@ -124,10 +121,23 @@ private:
         { "", "rng_4", &state.rng[3], DATA_FLOAT, false },
         { "", "rng_5", &state.rng[4], DATA_FLOAT, false },
         { "", "rng_6", &state.rng[5], DATA_FLOAT, false },
+        {"","velocity_wind", &state.velocity_wind, DATA_VECTOR3F, false},
         {"windvane","direction", &state.wind_vane_apparent.direction, DATA_FLOAT, false},
         {"windvane","speed", &state.wind_vane_apparent.speed, DATA_FLOAT, false},
         {"", "airspeed", &state.airspeed, DATA_FLOAT, false},
         {"", "no_time_sync", &state.no_time_sync, BOOLEAN, false},
+        { "rc", "rc_1", &state.rc[0], DATA_FLOAT, false },
+        { "rc", "rc_2", &state.rc[1], DATA_FLOAT, false },
+        { "rc", "rc_3", &state.rc[2], DATA_FLOAT, false },
+        { "rc", "rc_4", &state.rc[3], DATA_FLOAT, false },
+        { "rc", "rc_5", &state.rc[4], DATA_FLOAT, false },
+        { "rc", "rc_6", &state.rc[5], DATA_FLOAT, false },
+        { "rc", "rc_7", &state.rc[6], DATA_FLOAT, false },
+        { "rc", "rc_8", &state.rc[7], DATA_FLOAT, false },
+        { "rc", "rc_9", &state.rc[8], DATA_FLOAT, false },
+        { "rc", "rc_10", &state.rc[9], DATA_FLOAT, false },
+        { "battery", "voltage", &state.bat_volt, DATA_FLOAT, false },
+        { "battery", "current", &state.bat_amp, DATA_FLOAT, false },
     };
 
     // Enum coresponding to the ordering of keys in the keytable.
@@ -145,10 +155,23 @@ private:
         RNG_4       = 1U << 10,
         RNG_5       = 1U << 11,
         RNG_6       = 1U << 12,
-        WIND_DIR    = 1U << 13,
-        WIND_SPD    = 1U << 14,
-        AIRSPEED    = 1U << 15,
-        TIME_SYNC   = 1U << 16,
+        WIND_VEL    = 1U << 13,
+        WIND_DIR    = 1U << 14,
+        WIND_SPD    = 1U << 15,
+        AIRSPEED    = 1U << 16,
+        TIME_SYNC   = 1U << 17,
+        RC_1        = 1U << 18,
+        RC_2        = 1U << 19,
+        RC_3        = 1U << 20,
+        RC_4        = 1U << 21,
+        RC_5        = 1U << 22,
+        RC_6        = 1U << 23,
+        RC_7        = 1U << 24,
+        RC_8        = 1U << 25,
+        RC_9        = 1U << 26,
+        RC_10       = 1U << 27,
+        BAT_VOLT    = 1U << 28,
+        BAT_AMP     = 1U << 29,
     };
     uint32_t last_received_bitmask;
 };
