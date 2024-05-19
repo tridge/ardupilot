701da7ee09eb070c8bcb5ed67b6089925404d8a4
Andrew Tridgell <andrew@tridgell.net>
2024-05-20 05:57:35 +1000

    waf: pthread link not needed for QURT

--- Tools/ardupilotwaf/boards.py
+++ Tools/ardupilotwaf/boards.py
@@ -1696,7 +1696,6 @@ class QURT(Board):
             "--wrap=memalign",
             "--wrap=__stack_chk_fail",
             "-lc",
-            '-lpthread',
             f"-soname={cfg.env.HEXAGON_APP}",
             "--start-group",
             "--whole-archive",
