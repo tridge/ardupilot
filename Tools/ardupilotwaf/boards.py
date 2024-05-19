b808b856bbbdc442a7c258723aab055d079505c8
Andrew Tridgell <andrew@tridgell.net>
2024-05-19 19:39:09 +1000

    waf: added -lpthread for QURT

--- Tools/ardupilotwaf/boards.py
+++ Tools/ardupilotwaf/boards.py
@@ -1696,6 +1696,7 @@ class QURT(Board):
             "--wrap=memalign",
             "--wrap=__stack_chk_fail",
             "-lc",
+            '-lpthread',
             f"-soname={cfg.env.HEXAGON_APP}",
             "--start-group",
             "--whole-archive",
