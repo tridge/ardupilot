aa2928d1ac8ca753dc9c3b2d12d828030bb664c9
Bob Long <robertlong13@gmail.com>
2024-06-06 17:09:55 +1000

    carbonix_build: build CPN variants based on .parm
    Added Volanti cpn boards in Carbonix CarbonixCommon hwdef folder.
    Modified Carbonix build script and workflow to build CPN variants based
    on .parm files.
    
    SW-159

--- Tools/Carbonix_scripts/carbonix_waf_build.sh
+++ Tools/Carbonix_scripts/carbonix_waf_build.sh
@@ -3,39 +3,40 @@
 # Exit if any command fails
 set -e
 
-# Check unstaged changes
-if [[ $(git status --porcelain) ]]; then
-    echo "Error: There are unstaged changes in the repository."
-    exit 1
-fi
-
-# Check outdated submodules
-# git submodule update --remote --quiet
-# outdated_submodules=$(git submodule status | grep -e '-[[:alnum:]]' | awk '{print $2}')
-# if [[ -n $outdated_submodules ]]; then
-#     echo "The following submodules are not updated:"
-#     echo "$outdated_submodules"
-#     exit 1
-# fi
-
 echo "Running distclean..."
 ./waf distclean
 
-main_boards=("CubeOrange-Volanti" "CubeOrange-Ottano" "CubeOrange" "sitl")
-
+main_boards=("CubeOrange" "CubeOrange-Volanti" "CubeOrange-Ottano" "sitl")
 for board in "${main_boards[@]}"; do
-  echo "Compiling Plane for $board..."
+  echo "Compiling ArduPlane for $board..."
   ./waf configure --board "$board" --define=CARBOPILOT=1
   ./waf plane
 done
+echo "Script completed successfully."
 
 periph_boards=("CarbonixF405" "CarbonixF405-no-crystal")
-
 for board in "${periph_boards[@]}"; do
-  echo "Compiling AP_Periph for $board..."
-  ./waf configure --board "$board" --define=CARBOPILOT=1
-  ./waf AP_Periph
-done
+  for file in libraries/AP_HAL_ChibiOS/hwdef/CarbonixCommon/cpn_params/*.parm; do
+    # Extract the filename without the extension
+    filename=$(basename -- "$file")
+    filename="${filename%.*}"
 
+    # Create extra hwdef file
+    printf "undef CAN_APP_NODE_NAME\ndefine CAN_APP_NODE_NAME \"$board-$filename\"" > temp.hwdef
+    
+    # Compile AP_Periph for each board
+    echo "Compiling AP_Periph for $board with $filename..."
+    ./waf configure --board "$board" --define=CARBOPILOT=1 --extra-hwdef=temp.hwdef --default-parameters="$file"
+    ./waf AP_Periph
+    
+    # Rename build outputs
+    mkdir build/$board/bin/$filename
+    # Move all the files (not folders) in build/$board/bin to build/$board/bin/$filename
+    find build/$board/bin -maxdepth 1 -type f -exec mv {} build/$board/bin/$filename \;
+
+    # Cleanup
+    rm temp.hwdef
+  done
+done
 
 echo "Script completed successfully."
