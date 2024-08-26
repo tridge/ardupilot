462289da34cfecc61a04ca28e7fb91ce0b7e8c42
Tom Pittenger <tom.pittenger@krausaerospace.com>
2024-08-26 09:19:34 -0700

    AP_Vehicle: Add prev_distance to Rangefinder_State

--- libraries/AP_Vehicle/AP_Vehicle.h
+++ libraries/AP_Vehicle/AP_Vehicle.h
@@ -199,6 +199,9 @@ public:
     virtual bool get_target_location(Location& target_loc) { return false; }
     virtual bool update_target_location(const Location &old_loc, const Location &new_loc) { return false; }
 
+    virtual bool get_airspeed_min(float &airspeed_min) { return false; }
+    virtual bool get_airspeed_max(float &airspeed_max) { return false; }
+
     // circle mode controls (only used by scripting with Copter)
     virtual bool get_circle_radius(float &radius_m) { return false; }
     virtual bool set_circle_rate(float rate_dps) { return false; }
