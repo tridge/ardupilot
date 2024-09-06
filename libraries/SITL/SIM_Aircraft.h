0aec005e6dd4df5fda68ca2f5010fc0e4a08070f
Andrew Tridgell <andrew@tridgell.net>
2024-09-06 14:39:26 +1000

    SITL: modify for quad motors on first 4 outputs

--- libraries/SITL/SIM_Aircraft.h
+++ libraries/SITL/SIM_Aircraft.h
@@ -318,7 +318,7 @@ private:
         Location location;
     } smoothing;
 
-    LowPassFilterFloat servo_filter[5];
+    LowPassFilterFloat servo_filter[16];
 
     Buzzer *buzzer;
     Sprayer *sprayer;
