0395ee6e2d621b68bbab761230b5da272d587b52
Paul Riseborough <gncsolns@gmail.com>
2024-09-08 10:20:46 +1000

    AP_NavEKF2: restrict airspeed use to real measurements > 10
    Allows synthetic airspeed to be used for flight control but not wind estimation.
    Threshold increased to allow for larger sensor offsets.

--- libraries/AP_NavEKF2/AP_NavEKF2_Control.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2_Control.cpp
@@ -369,7 +369,8 @@ void NavEKF2_core::checkAttitudeAlignmentStatus()
 // return true if we should use the airspeed sensor
 bool NavEKF2_core::useAirspeed(void) const
 {
-    return dal.airspeed_sensor_enabled();
+    const auto *aspeed = dal.airspeed();
+    return aspeed && dal.airspeed_sensor_enabled() && !dal.airspeed_is_synthetic() && aspeed->get_airspeed() > 10.0f;
 }
 
 // return true if we should use the range finder sensor
