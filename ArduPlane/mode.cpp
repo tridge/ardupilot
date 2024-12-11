cc365dd755b737e8a0028aaf9cd78733aeb190b5
Andrew Tridgell <andrew@tridgell.net>
2024-12-11 16:57:30 +1100

    Plane: reset landing on mode entry

--- ArduPlane/mode.cpp
+++ ArduPlane/mode.cpp
@@ -142,6 +142,9 @@ bool Mode::enter()
         // Make sure the flight stage is correct for the new mode
         plane.update_flight_stage();
 
+        // reset landing state
+        plane.landing.reset();
+
 #if HAL_QUADPLANE_ENABLED
         if (quadplane.enabled()) {
             float aspeed;
