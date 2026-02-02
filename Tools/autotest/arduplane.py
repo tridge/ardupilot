ffff6700f3d5258626b4070810ed2c2d2efa4623
Rubén Pérez Alonso <rperez@ventorinnovations.es>
2026-02-02 14:15:24 +0100

    autotest: increase wait_altitude timeouts

--- Tools/autotest/arduplane.py
+++ Tools/autotest/arduplane.py
@@ -1692,6 +1692,7 @@ class AutoTestPlane(vehicle_test_suite.TestSuite):
         })
         self.wait_altitude(altitude_min=return_alt-3,
                            altitude_max=return_alt+3,
+                           timeout=60,
                            relative=True)
         self.wait_circling_point_with_radius(fence_loc, return_radius)
         self.do_fence_disable() # Disable fence so we can land
@@ -6642,7 +6643,7 @@ class AutoTestPlane(vehicle_test_suite.TestSuite):
             higher_home = copy.copy(home)
             higher_home.alt += 40
             self.set_home(higher_home)
-            self.wait_altitude(home.alt+target_alt-5, home.alt+target_alt+5, relative=False, minimum_duration=10, timeout=12)
+            self.wait_altitude(home.alt+target_alt-5, home.alt+target_alt+5, relative=False, minimum_duration=10, timeout=30)
             self.disarm_vehicle(force=True)
             self.reboot_sitl()
 
