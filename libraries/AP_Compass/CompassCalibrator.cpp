akashbinu699/drone

AP_Compass: avoid having last element copied over itself
from PR review

libraries/AP_Compass/CompassCalibrator.cpp
@@ -523,7 +523,9 @@ void CompassCalibrator::thin_samples()
     while (i != _samples_collected) {
         if (!accept_sample(_sample_buffer[i], i)) {
             // throw sample away
-            _sample_buffer[i] = _sample_buffer[_samples_collected-1];
+            if (i != _samples_collected-1) {  // do not copy over outselves!
+                _sample_buffer[i] = _sample_buffer[_samples_collected-1];
+            }
             _samples_collected--;
             _samples_thinned++;
             continue;

tridge/ardupilot

libraries/AP_Compass/CompassCalibrator.cpp
@@ -521,7 +521,9 @@ void CompassCalibrator::thin_samples()
     while (i != _samples_collected) {
         if (!accept_sample(_sample_buffer[i], i)) {
             // throw sample away
-            _sample_buffer[i] = _sample_buffer[_samples_collected-1];
+            if (i != _samples_collected-1) {  // do not copy over ourselves!
+                _sample_buffer[i] = _sample_buffer[_samples_collected-1];
+            }
             _samples_collected--;
             _samples_thinned++;
             continue;